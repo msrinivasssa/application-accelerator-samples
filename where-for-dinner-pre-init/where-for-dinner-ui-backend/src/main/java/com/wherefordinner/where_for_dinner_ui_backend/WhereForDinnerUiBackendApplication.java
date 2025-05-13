fda865186acf6523a2625867b1a82747177cd21f
Madhavan <msrinivasssa@vmware.com>
2025-05-13 18:42:56 +0530

    Adding backend service for UI

---
+++ where-for-dinner-pre-init/where-for-dinner-ui-backend/src/main/java/com/wherefordinner/where_for_dinner_ui_backend/WhereForDinnerUiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wherefordinner.where_for_dinner_ui_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereForDinnerUiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereForDinnerUiBackendApplication.class, args);
+	}
+
+}
---
+++ where-for-dinner-pre-init/where-for-dinner-ui-backend/src/test/java/com/wherefordinner/where_for_dinner_ui_backend/WhereForDinnerUiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wherefordinner.where_for_dinner_ui_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereForDinnerUiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
